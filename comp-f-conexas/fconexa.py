5e6a58add5b372f8ff3693136e8c26e2ed567c0f
anamachadoc <anaclaudiamachado211@gmail.com>
2023-09-24 17:53:45 -0300

    finale version

--- comp-f-conexas/fconexa.py
+++ comp-f-conexas/fconexa.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random as rd
 import numpy as np
 
 # reading an directed graph
@@ -7,7 +8,7 @@ G = nx.read_edgelist("graph.txt", create_using=nx.DiGraph())
 vertices = list(G.nodes)
 edges = list(G.edges)
 
-# plotting the graph
+# plotting the input graph
 nx.draw_circular(
     G, 
     with_labels = True,
@@ -51,18 +52,45 @@ while len(vertices) != 0:
             auxiliar.append(vt)
     result.append (component)
     vertices = auxiliar # only vertices that aren't in the component remain in the vertice list
+
+# function to not allow colors to repeat
+def define_color (color_map): 
+    color = "#"+''.join([rd.choice('0123456789ABCDEF') for j in range(6)]) 
+    if color in color_map:
+        define_color (color_map)
+    else:
+        return color
     
 # output
-output = open("comp-f-conexas.txt", 'w')
-print ("\n ha", len(result), "componentes f-conexas! sao elas:\n")  
-output.write (str(len(result))+" componentes f-conexas:\n")
-for compf in result:
-    print(compf, "\n")
+S = nx.DiGraph ()
+color_map = [] # one color for each strongly connected component
+output = open("comp-f-conexas.txt", 'w') # .txt file to write the output
+print ("\n ha {} componente(s) f-conexa(s):" .format(len(result))) # print to terminal 
+output.write (str(len(result))+" componente(s) f-conexa(s):\n")
+
+for i in range(len(result)):  
+    color = define_color (color_map) # generating RGB code
+    print(result[i])
     output.write ("[ ")
-    for vert in compf:
-        output.write("'" + vert + "' ")
+    for vert in result[i]:
+        S.add_node(vert) # adding the vertice to the output graph
+        color_map.append (color) # adding the vertice color 
+        output.write("'" + vert + "' ") # writing on file
     output.write ("]")
     output.write("\n")
-output.close() 
+output.close ()    
+S.add_edges_from (edges) # adding the set of edges to the output graph
 
-  
+# graph S
+nx.draw_circular(
+    S,
+    with_labels = True,
+    node_size=200,
+    node_color= color_map,
+    )
+
+plt.savefig ("output-graph.jpg") # output graph image
+
+
+
+
