67b436620d82a1fbed0e7cd31ac244cf51d700ea
Ana Cláudia Machado <108549971+anamachadoc@users.noreply.github.com>
2023-11-14 20:18:40 -0300

    Finalized Ford Fulkerson algorithm and documentation

--- fluxo/auxiliar.py
+++ fluxo/auxiliar.py
@@ -1,6 +1,6 @@
 G = {} # G eh o grafo de folgas
 
-# todo vertice possui um dicionario com seus adjacentes e os valores das arestas que os ligam
+# todo vertice possui um dicionario com seus adjacentes e os valores de fluxo das arestas que os ligam
 def criarVertice(v): 
     if v not in G.keys():
         G[v] ={}
@@ -9,62 +9,35 @@ def adicionarAresta(v, u, fluxo):
     G[v][u] = int(fluxo)
     G[u][v] = int(0)
     
-def zerarVisitados (visitados, G):
+def zerarVisitados (visitado, G):
     v = list(G.keys())
     for vertice in v:
-        visitados[vertice] = False
+        visitado[vertice] = False
        
-def inserirChaves (anteriores, G):
+def inserirChaves (antecessor, G):
     v = list(G.keys())
     for vertice in v:
-        anteriores[vertice] = 'None'
+        antecessor[vertice] = 'None'
         
 def passarFluxo (G, inicio, fim, valorFluxo):
     G[inicio][fim] += valorFluxo
     
 # busca em largura para determinar os caminhos
-def BFS(G, s, t, anteriores):
-    visitados = {}
-    zerarVisitados (visitados, G) # a cada novo caminho, todos os vertices devem estar com a chave visitado como False
-    fifo = [] # fila que auxilia na montagem do caminho
+def BFS(G, s, t, antecessor):
+    visitado = {}
+    zerarVisitados (visitado, G) # a cada novo caminho, todos os vertices devem estar com a chave visitado como False
+    fifo = []
     fifo.append(s) # primeiro elemento eh a origem
-    visitados[s] = True
+    visitado[s] = True
  
     while fifo:
         u = fifo.pop(0)
         adj = list(G[u].keys())
         for v in adj: # olha os adjacentes do vertice removido - primeiro da fila
             # adiciona na fila os possiveis = nao visitado e possivel de passar fluxo
-            if visitados[v] is False and G[u][v] > 0:
+            if visitado[v] is False and G[u][v] > 0:
                 fifo.append(v)
-                visitados[v] = True
-                anteriores[v] = u # define o anterior ao elemento adicionado
+                visitado[v] = True
+                antecessor[v] = u # define o anterior ao elemento adicionado
     # se chegou ate T entao retorna o caminho - se nao quer dizer que todos os caminhos possiveis ja foram olhados
-    return True if visitados[t] else False
-
-def FordFulkerson(G, origem, fim):
-    # anteriores eh utilizado para saber por onde chegou-se ate determinado vertice - dicionario com todos os vertices de G
-    anteriores = {}
-    inserirChaves (anteriores, G)
-    fluxoMaximo = 0
-    while BFS(G, origem, fim, anteriores):
-        aux = fim
-        fluxoCaminho = G[anteriores[aux]][aux]
-
-        # encontrando o menor valor de fluxo naquele caminho = o fluxo que pode passar
-        while aux != origem: 
-            fluxoCaminho = min(fluxoCaminho, G[anteriores[aux]][aux])
-            aux = anteriores[aux]
- 
-        # somando ao valor de fluxo maximo
-        fluxoMaximo += fluxoCaminho
-        v = fim
- 
-        # atualizando valores no grafo de folgas
-        while v != origem:
-            u = anteriores[v]
-            passarFluxo (G, v, u, fluxoCaminho)
-            passarFluxo (G, u, v, -fluxoCaminho)
-            v = anteriores[v]
-            
-    return fluxoMaximo
+    return True if visitado[t] else False
--- fluxo/fluxoMaximo.py
+++ fluxo/fluxoMaximo.py
@@ -1,5 +1,6 @@
 import auxiliar as a
         
+# leitura do arquivo de entrada
 graph = open('graph.txt')
 for line in graph.readlines():
     line = line.split()
@@ -9,12 +10,36 @@ for line in graph.readlines():
     a.criarVertice(inicio)
     a.criarVertice(fim)
     a.adicionarAresta (inicio, fim, fluxo)
+    
+def FordFulkerson(G, origem, fim):
+    # antecessor eh utilizado para saber por onde chegou-se ate determinado vertice 
+    antecessor = {}
+    a.inserirChaves (antecessor, G)
+    fluxoMaximo = 0
+    while a.BFS(G, origem, fim, antecessor):
+        aux = fim
+        fluxoCaminho = G[antecessor[aux]][aux]
+        # encontrando o menor valor de fluxo naquele caminho = o fluxo que pode passar
+        while aux != origem: 
+            fluxoCaminho = min(fluxoCaminho, G[antecessor[aux]][aux])
+            aux = antecessor[aux]
+        # somando ao valor de fluxo maximo
+        fluxoMaximo += fluxoCaminho
+        v = fim
+        # atualizando valores no grafo de folgas
+        while v != origem:
+            u = antecessor[v]
+            a.passarFluxo (G, v, u, fluxoCaminho)
+            a.passarFluxo (G, u, v, -fluxoCaminho)
+            v = antecessor[v]      
+    return fluxoMaximo
 
-fluxo = a.FordFulkerson(a.G, 'S', 'T')
-print (f'- o fluxo maximo eh {fluxo}')
+fluxo = FordFulkerson(a.G, 'S', 'T')
+
+print (f'\nO fluxo maximo eh {fluxo}\n')
 print ('---------- FLUXO ELEMENTAR EM CADA ARESTA ----------')
 vertices = a.G.keys()
 for v in vertices:
     adj = a.G[v].keys()
     for u in adj:
-        print (f' {v}-{u}: {a.G[v][u]} ||', end = '')
+        print (f'{v}-{u}: {a.G[v][u]}')
